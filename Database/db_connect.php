60f2aee03fd82134a2879617c8b461585c2596f7
ThuHuong <157537612+thh21202@users.noreply.github.com>
2026-04-27 04:22:00 -0500

    Update database connection password placeholder

--- Database/db_connect.php
+++ Database/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "Duckymomo2002!", "alpha_pizza_db");
+$conn = new mysqli("localhost", "root", "YOUR_MYSQL_PASSWORD", "alpha_pizza_db");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
